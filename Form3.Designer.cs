5244383063b3918b62a86d3f69c90e10d201bc88
AHussienAdel <143330087+AHussienAdel@users.noreply.github.com>
2024-05-30 03:53:24 +0300

    Add files via upload
     A classical inventory problem concerns the purchase and sale of newspapers.
     The paper seller buys the papers for 33 cents each and sells them for 50 cents each. 
     Newspapers not sold at the end of the day are sold as scrap for 5 cents each.
     Newspapers can be purchased in bundles of 10. Thus, the paper seller can buy 50, 60, and so on. 
    There are three types of newsdays, “good,” “fair,” and “poor”. Each having its own probability.
     The problem is to determine the optimal number of papers the newspaper seller should purchase. 
     This will be accomplished by simulating demands for 20 days and recording profits from sales each day
    
    System Input:
     Newspaper Purchase Price
     Newspaper Selling Price
     Newspaper Scrap Value
     Number of Papers Purchased by News Dealer      
     Probability distribution of the predefined news day types (Good, Fair, Poor)
     The demand probability distribution for each day type
    
    
    System Output:
    1. Simulation table with the columns below:
    Day index
    random number for day type
    day type
    random number for demand
    demand (based on day type)
    revenue from sales
    lost profit from excess demand
    salvage from sale of scrap
    daily profit
    2. Performance measures:
    Total Sales Revenue
    Total Cost of Newspapers
    Total Lost Profit from Excess Demand
    Total Salvage from sale of Scrap papers
    Net Profit
    Number of days having excess demand
    Number of days having unsold papers

---
+++ Form3.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace MultiQueueSimulation
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DaysWithUnsoldPapers = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DaysWithMoreDemand = new System.Windows.Forms.TextBox();
+            this.TotalNetProfit = new System.Windows.Forms.TextBox();
+            this.TotalScrapProfit = new System.Windows.Forms.TextBox();
+            this.TotalLostProfit = new System.Windows.Forms.TextBox();
+            this.TotalCost = new System.Windows.Forms.TextBox();
+            this.TotalSalesProfit = new System.Windows.Forms.TextBox();
+            this.TotalNetProfi = new System.Windows.Forms.Label();
+            this.TotalScrapProfi = new System.Windows.Forms.Label();
+            this.TotalLostProfi = new System.Windows.Forms.Label();
+            this.TotalCos = new System.Windows.Forms.Label();
+            this.TotalSalesProfi = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.testcase1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DaysWithUnsoldPapers);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.DaysWithMoreDemand);
+            this.tabPage2.Controls.Add(this.TotalNetProfit);
+            this.tabPage2.Controls.Add(this.TotalScrapProfit);
+            this.tabPage2.Controls.Add(this.TotalLostProfit);
+            this.tabPage2.Controls.Add(this.TotalCost);
+            this.tabPage2.Controls.Add(this.TotalSalesProfit);
+            this.tabPage2.Controls.Add(this.TotalNetProfi);
+            this.tabPage2.Controls.Add(this.TotalScrapProfi);
+            this.tabPage2.Controls.Add(this.TotalLostProfi);
+            this.tabPage2.Controls.Add(this.TotalCos);
+            this.tabPage2.Controls.Add(this.TotalSalesProfi);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1091, 433);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DaysWithUnsoldPapers
+            // 
+            this.DaysWithUnsoldPapers.Location = new System.Drawing.Point(208, 260);
+            this.DaysWithUnsoldPapers.Name = "DaysWithUnsoldPapers";
+            this.DaysWithUnsoldPapers.Size = new System.Drawing.Size(100, 22);
+            this.DaysWithUnsoldPapers.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "DaysWithUnsoldPapers";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "show performance metrices";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "DaysWithMoreDemand";
+            // 
+            // DaysWithMoreDemand
+            // 
+            this.DaysWithMoreDemand.Location = new System.Drawing.Point(208, 216);
+            this.DaysWithMoreDemand.Name = "DaysWithMoreDemand";
+            this.DaysWithMoreDemand.Size = new System.Drawing.Size(100, 22);
+            this.DaysWithMoreDemand.TabIndex = 12;
+            // 
+            // TotalNetProfit
+            // 
+            this.TotalNetProfit.Location = new System.Drawing.Point(208, 176);
+            this.TotalNetProfit.Name = "TotalNetProfit";
+            this.TotalNetProfit.Size = new System.Drawing.Size(100, 22);
+            this.TotalNetProfit.TabIndex = 11;
+            // 
+            // TotalScrapProfit
+            // 
+            this.TotalScrapProfit.Location = new System.Drawing.Point(208, 142);
+            this.TotalScrapProfit.Name = "TotalScrapProfit";
+            this.TotalScrapProfit.Size = new System.Drawing.Size(100, 22);
+            this.TotalScrapProfit.TabIndex = 8;
+            // 
+            // TotalLostProfit
+            // 
+            this.TotalLostProfit.Location = new System.Drawing.Point(208, 97);
+            this.TotalLostProfit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TotalLostProfit.Name = "TotalLostProfit";
+            this.TotalLostProfit.Size = new System.Drawing.Size(100, 22);
+            this.TotalLostProfit.TabIndex = 6;
+            // 
+            // TotalCost
+            // 
+            this.TotalCost.Location = new System.Drawing.Point(208, 58);
+            this.TotalCost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TotalCost.Name = "TotalCost";
+            this.TotalCost.Size = new System.Drawing.Size(100, 22);
+            this.TotalCost.TabIndex = 5;
+            // 
+            // TotalSalesProfit
+            // 
+            this.TotalSalesProfit.Location = new System.Drawing.Point(208, 18);
+            this.TotalSalesProfit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TotalSalesProfit.Name = "TotalSalesProfit";
+            this.TotalSalesProfit.Size = new System.Drawing.Size(100, 22);
+            this.TotalSalesProfit.TabIndex = 4;
+            // 
+            // TotalNetProfi
+            // 
+            this.TotalNetProfi.AutoSize = true;
+            this.TotalNetProfi.Location = new System.Drawing.Point(32, 176);
+            this.TotalNetProfi.Name = "TotalNetProfi";
+            this.TotalNetProfi.Size = new System.Drawing.Size(89, 16);
+            this.TotalNetProfi.TabIndex = 10;
+            this.TotalNetProfi.Text = "TotalNetProfit";
+            // 
+            // TotalScrapProfi
+            // 
+            this.TotalScrapProfi.AutoSize = true;
+            this.TotalScrapProfi.Location = new System.Drawing.Point(28, 145);
+            this.TotalScrapProfi.Name = "TotalScrapProfi";
+            this.TotalScrapProfi.Size = new System.Drawing.Size(104, 16);
+            this.TotalScrapProfi.TabIndex = 9;
+            this.TotalScrapProfi.Text = "TotalScrapProfit";
+            // 
+            // TotalLostProfi
+            // 
+            this.TotalLostProfi.AutoSize = true;
+            this.TotalLostProfi.Location = new System.Drawing.Point(28, 97);
+            this.TotalLostProfi.Name = "TotalLostProfi";
+            this.TotalLostProfi.Size = new System.Drawing.Size(93, 16);
+            this.TotalLostProfi.TabIndex = 3;
+            this.TotalLostProfi.Text = "TotalLostProfit";
+            // 
+            // TotalCos
+            // 
+            this.TotalCos.AutoSize = true;
+            this.TotalCos.Location = new System.Drawing.Point(37, 62);
+            this.TotalCos.Name = "TotalCos";
+            this.TotalCos.Size = new System.Drawing.Size(65, 16);
+            this.TotalCos.TabIndex = 2;
+            this.TotalCos.Text = "TotalCost";
+            // 
+            // TotalSalesProfi
+            // 
+            this.TotalSalesProfi.AutoSize = true;
+            this.TotalSalesProfi.Location = new System.Drawing.Point(28, 22);
+            this.TotalSalesProfi.Name = "TotalSalesProfi";
+            this.TotalSalesProfi.Size = new System.Drawing.Size(103, 16);
+            this.TotalSalesProfi.TabIndex = 1;
+            this.TotalSalesProfi.Text = "TotalSalesProfit";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.testcase1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(1091, 433);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // testcase1
+            // 
+            this.testcase1.Location = new System.Drawing.Point(63, 28);
+            this.testcase1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.testcase1.Name = "testcase1";
+            this.testcase1.Size = new System.Drawing.Size(107, 23);
+            this.testcase1.TabIndex = 1;
+            this.testcase1.Text = "show";
+            this.testcase1.UseVisualStyleBackColor = true;
+            this.testcase1.Click += new System.EventHandler(this.testcase1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 63);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(899, 345);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(28, 20);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1099, 462);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 503);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DaysWithMoreDemand;
+        private System.Windows.Forms.TextBox TotalNetProfit;
+        private System.Windows.Forms.TextBox TotalScrapProfit;
+        private System.Windows.Forms.TextBox TotalLostProfit;
+        private System.Windows.Forms.TextBox TotalCost;
+        private System.Windows.Forms.TextBox TotalSalesProfit;
+        private System.Windows.Forms.Label TotalNetProfi;
+        private System.Windows.Forms.Label TotalScrapProfi;
+        private System.Windows.Forms.Label TotalLostProfi;
+        private System.Windows.Forms.Label TotalCos;
+        private System.Windows.Forms.Label TotalSalesProfi;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button testcase1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox DaysWithUnsoldPapers;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using NewspaperSellerModels;
+namespace MultiQueueSimulation
+{
+    public partial class Form2 : Form
+    {
+        private SimulationSystem simulationSystem;
+
+        private string path;
+
+        public Form2(SimulationSystem system, string path)
+        {
+            InitializeComponent();
+            this.simulationSystem = system;
+            this.path = path;
+
+        }
+
+        private void RunSimulationButton_Click(object sender, EventArgs e)
+        {
+            DisplayResultsInDataGridView();
+            DisplayPerformanceMetrics();
+        }
+
+        private void DisplayResultsInDataGridView()
+        {
+            // Assuming you have a form with a DataGridView named dataGridView1
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Add columns to DataGridView
+            dataGridView1.Columns.Add("DayNo", " DayNo");
+            dataGridView1.Columns.Add("RandomNewsDayType", " RandomNewsDayType");
+            dataGridView1.Columns.Add("RandomDemand", " RandomDemand");
+            dataGridView1.Columns.Add("Demand", "Demand ");
+            dataGridView1.Columns.Add("SalesProfit", "  SalesProfit ");
+            dataGridView1.Columns.Add("LostProfit", "LostProfit ");
+            dataGridView1.Columns.Add("ScrapProfit", " ScrapProfit");
+            dataGridView1.Columns.Add("DailyNetProfit", "DailyNetProfit ");
+           
+            // Add rows to DataGridView
+            foreach (var simulationCase in simulationSystem.SimulationTable)
+            {
+
+                dataGridView1.Rows.Add(
+               simulationCase.DayNo,
+               simulationCase.RandomNewsDayType,
+               simulationCase.NewsDayType,
+               simulationCase.RandomDemand,
+               simulationCase.Demand,
+               simulationCase.SalesProfit,
+               simulationCase.LostProfit,
+               simulationCase.ScrapProfit,
+               simulationCase.DailyNetProfit
+               )
+           ;
+            }
+
+
+            dataGridView1.Refresh();
+
+        }
+        private void DisplayPerformanceMetrics()
+        {
+
+
+            TotalSalesProfit.Text = simulationSystem.PerformanceMeasures.TotalSalesProfit.ToString();
+            TotalCost.Text = simulationSystem.PerformanceMeasures.TotalCost.ToString();
+            TotalLostProfit.Text = simulationSystem.PerformanceMeasures.TotalLostProfit.ToString();
+            TotalScrapProfit.Text = simulationSystem.PerformanceMeasures.TotalScrapProfit.ToString();
+            TotalNetProfit.Text = simulationSystem.PerformanceMeasures.TotalNetProfit.ToString();
+            DaysWithMoreDemand.Text = simulationSystem.PerformanceMeasures.DaysWithMoreDemand.ToString();
+            DaysWithUnsoldPapers.Text = simulationSystem.PerformanceMeasures.DaysWithUnsoldPapers.ToString();
+        }
+
+   
+        private void testcase1_Click(object sender, EventArgs e)
+        {
+            DisplayResultsInDataGridView();
+        }
+
+     
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DisplayPerformanceMetrics();
+    }
+    }
+}
